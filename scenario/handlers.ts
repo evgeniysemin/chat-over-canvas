1a9bee96a26f295df6c883f2067d4a55748f59b0
Evgeniy Semin <evgeniy.o.semin@gmail.com>
2022-10-14 17:01:52 +0300

    Добавлен возврат карточек после любого сообщения.

--- scenario/handlers.ts
+++ scenario/handlers.ts
@@ -1,8 +1,8 @@
-import { SaluteHandler, SaluteRequest } from '@salutejs/scenario';
+import { SaluteHandler, SaluteRequest, SaluteResponse } from '@salutejs/scenario';
 
 import { TEXT_WITHOUT_FORCE_EXPAND, TEXT_WITH_FORCE_EXPAND, CARD } from './intents';
 
-export const runAppHandler: SaluteHandler<SaluteRequest> = ({ res }) => {
+const appendSuggestions = (res: SaluteResponse) => {
     res.appendSuggestions([
         TEXT_WITHOUT_FORCE_EXPAND,
         TEXT_WITH_FORCE_EXPAND,
@@ -10,19 +10,26 @@ export const runAppHandler: SaluteHandler<SaluteRequest> = ({ res }) => {
     ]);
 };
 
+export const runAppHandler: SaluteHandler<SaluteRequest> = ({ res }) => {
+    appendSuggestions(res);
+};
+
 export const noMatchHandler: SaluteHandler = ({ res }) => {
     res.setPronounceText('Ха-ха! Ох, как смешно!');
     res.appendBubble('Ха-ха! Ох, как смешно!');
+    appendSuggestions(res);
 };
 
 export const handleTextWithoutForceExpand: SaluteHandler<SaluteRequest> = ({ res }) => {
     res.setPronounceText('Текстовое сообщение без принудительного открытия');
     res.appendBubble('Текстовое сообщение без принудительного открытия');
+    appendSuggestions(res);
 };
 
 export const handleTextWithForceExpand: SaluteHandler<SaluteRequest> = ({ res }) => {
     res.setPronounceText('Текстовое сообщение с принудительным открытием');
     res.appendBubble('Текстовое сообщение с принудительным открытием', { expand_policy: 'force_expand' });
+    appendSuggestions(res);
 };
 
 export const handleCard: SaluteHandler<SaluteRequest> = ({ res }) => {
@@ -34,5 +41,6 @@ export const handleCard: SaluteHandler<SaluteRequest> = ({ res }) => {
                 left: { type: 'simple_left_view' },
             },
         ],
-    })
+    });
+    appendSuggestions(res);
 };
